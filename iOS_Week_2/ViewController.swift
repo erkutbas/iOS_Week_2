c22fd5c1f9a9219b98d3931d72787545f89cb4a2
erkutbas <74869943+erkutbas0@users.noreply.github.com>
2021-09-21 22:50:11 +0300

    Initial Commit

---
+++ iOS_Week_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Week_2
+//
+//  Created by Erkut Bas on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
